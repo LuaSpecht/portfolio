7f2c2fd8c6a9456b64aaeb3cd846c05544d5af88
Luana Specht <spechtdeveloper@gmail.com>
2025-02-15 21:27:18 -0300

    Style: fotter ready but with a placeholder site adress

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
   return(
     <footer>
-      <p>Opa</p>
+      <p>Copyright &#169; 2025<a href='#header'>&nbsp; luaspecht.com</a>. Todos os direitos reservados.</p>
     </footer>
   )
 }
