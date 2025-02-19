d0d634d6dd30deb4975aa68fac6b0dd5faa206c5
Luana Specht <spechtdeveloper@gmail.com>
2025-02-19 01:20:45 -0300

    fix: footer text resumed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
   return(
     <footer>
-      <p>Copyright &#169; 2025<a href='#header'>&nbsp; luaspecht.com</a>. Todos os direitos reservados.</p>
+      <p>Copyright &#169; 2025<a href='#header'>&nbsp; luaspecht.com</a></p>
     </footer>
   )
 }
