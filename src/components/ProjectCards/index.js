a02ed1df1829e3b6ce97b7a72b532b88c4ba5fbb
Luana Specht <spechtdeveloper@gmail.com>
2026-07-13 15:57:11 -0300

    feat: brand new project

--- src/components/ProjectCards/index.js
+++ src/components/ProjectCards/index.js
@@ -2,14 +2,14 @@ import './ProjectCards.css'
 import Card from '../Card'
 import weddingPrint from "../../assets/images/wedding-print.png";
 import reviewPrint from "../../assets/images/review-print.png";
-import doutoriaPrint from "../../assets/images/doutoria-print.png";
+import eccosPrint from "../../assets/images/eccos-print.png";
 
 const ProjectCards = (props) =>{
   return(
     <div className='carrousel'>
       <Card name='Wedding Site | Site de Casamento' tags='#JavaScript #Invite #Save-the-date' image={weddingPrint} projectLink='https://casamento-lua-e-nics.netlify.app/' githubLink='https://github.com/LuaSpecht/wedding-site' />
       <Card name='NicLua | Games Review' tags='#JavaScript #Games #Blog' image={reviewPrint} projectLink='https://luaspecht.github.io/NicLua/' githubLink='https://github.com/LuaSpecht/NicLua' />
-      <Card name='Doutoria | Projeto Prático da Faculdade' tags='#Faculdade #API #Resposividade' image={doutoriaPrint} projectLink='https://luaspecht.github.io/portfolio_pratico/' githubLink='https://github.com/LuaSpecht/portfolio_pratico' />
+      <Card name='ECCOS | Engenharia de Recursos Hídricos' tags='#ProjetosReais #Landing-Page' image={eccosPrint} projectLink='https://www.eccosengenharia.com.br/' githubLink='https://github.com/LuaSpecht/ECCOS' />
     </div>
   )
 }
